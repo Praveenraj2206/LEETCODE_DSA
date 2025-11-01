8fc987be6b1a4efd54c60105e6825853d784f4be
Praveen Raj S <praveenrajs2005@gmail.com>
2025-11-01 14:38:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (54.13%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        # output=''
+        # for i in s:
+        #     uppercase=i
+        #     if uppercase.isupper():
+        #         lowercase = chr(ord(uppercase) + 32)
+        #         output += lowercase
+        #     else:
+        #         output += uppercase
+        
+
+        # or
+
+
+        # for i in s:
+        #     uppercase=i
+        #     if uppercase.isupper():
+        #         output+=i.lower()
+        #     else:
+        #         output+=uppercase
+        # return output
+        
+
+        # or
+        return s.lower()
