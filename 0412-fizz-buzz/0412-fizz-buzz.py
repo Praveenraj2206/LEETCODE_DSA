a8f83ca5b6922f2d28da19524b78db850cc198f8
Praveen Raj S <praveenrajs2005@gmail.com>
2025-11-09 09:21:47 +0530

    Update 0412-fizz-buzz.py

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,19 +1,5 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        # answer=[]
-        # for i in range(1,n+1):
-        #     if i%3==0 and i%5==0:
-        #         answer.append('FizzBuzz')
-        #     elif i%5==0:
-        #         answer.append('Buzz')
-        #     elif i%3==0:
-        #         answer.append('Fizz')
-        #     else:
-        #         i=str(i)
-        #         answer.append(i)
-
-        # or
-
         answer = [
                   'FizzBuzz' if i % 3 == 0 and i % 5 == 0
                    else 'Buzz' if i % 5 == 0
