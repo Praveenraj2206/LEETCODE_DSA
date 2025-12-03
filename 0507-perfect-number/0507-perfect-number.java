577e9c581b265d48e73d7adc6c5eebb0a6af78ee
Praveen Raj S <praveenrajs2005@gmail.com>
2025-12-03 21:56:43 +0530

    Time: 1 ms (96.83%), Space: 42.4 MB (6.4%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 1) 
+        {
+            return false;
+        }
+
+        int total = 1; 
+        int i = 2;
+
+        while (i * i <= num) {
+            if (num % i == 0) {
+                total += i;
+                if (i != num / i) {  
+                    total += num / i;
+                }
+            }
+            i++;
+        }
+
+        return total == num;
+    }
+}
