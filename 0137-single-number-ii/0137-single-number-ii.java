1197a1f025f7f7cd7db6995cd9e566b60042d7a8
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-14 09:05:53 +0530

    Time: 1 ms (93.72%), Space: 45.4 MB (50.88%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public int singleNumber(int[] nums) 
+    {
+        int x=0,y=0;
+        for(int num:nums)
+        {
+            x=(x^num) & ~y;
+            y=(y^num) & ~x;
+        }
+        return x;
+    }
+}
