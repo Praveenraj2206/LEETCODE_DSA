5967e3b536be58cb1e277a85653e66d1a8572a28
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-14 09:07:25 +0530

    Time: 1 ms (93.72%), Space: 45.2 MB (86.53%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -3,10 +3,9 @@ class Solution
     public int singleNumber(int[] nums) 
     {
         int x=0,y=0;
-        for(int num:nums)
-        {
-            x=(x^num) & ~y;
-            y=(y^num) & ~x;
+        for(int num : nums){
+            x ^= (num & ~y);
+            y ^= (num & ~x);
         }
         return x;
     }
