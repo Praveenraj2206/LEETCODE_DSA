739d33e1c8a150794e165591f893961176415295
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-15 17:29:03 +0530

    Time: 10 ms (17.32%), Space: 12.6 MB (20.59%) - LeetHub

--- 3842-toggle-light-bulbs/3842-toggle-light-bulbs.py
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.py
@@ -6,5 +6,14 @@ class Solution(object):
             if value%2 != 0:
                 new.append(key)
         return sorted(new)
-            
-        
+
+
+    # def toggleLightBulbs(self, bulbs):
+    #     bulb_states = [False] * 101
+    #     for bulb_num in bulbs:
+    #         bulb_states[bulb_num] = not bulb_states[bulb_num]
+    #     result = []
+    #     for i in range(1, 101):
+    #         if bulb_states[i]:
+    #             result.append(i)
+    #     return result
