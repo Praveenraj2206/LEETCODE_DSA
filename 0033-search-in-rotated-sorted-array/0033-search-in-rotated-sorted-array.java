523c951419a16c158cf04cd1299e277e4318ded2
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-15 17:52:31 +0530

    Time: 0 ms (100%), Space: 44 MB (25.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -60,4 +60,50 @@ class Solution
         }
         return -1;
     }
+
+
+    // int findPivotwithduplicate(int[] arr)
+    // {
+    //     int start=0;
+    //     int end=arr.length-1;
+    //     while(start <= end)
+    //     {
+    //         int mid = start + (end-start)/2;
+    //         if(mid < end && arr[mid]>arr[mid+1])
+    //         {
+    //             return mid;
+    //         }
+    //         if(mid > start && arr[mid]<arr[mid-1])
+    //         {
+    //             return mid-1;
+    //         }
+
+    //         //if elements at mid,end,start are equal then just skip the duplicates
+    //         if (arr[mid] == arr[start] && arr[mid] == arr[end]) 
+    //         {
+    //             //skip the duplicates
+    //             //Note: what if these elements at start and end were the pivot?
+    //             //check if the start is pivot
+    //             if(arr[start] > arr[start+1])
+    //             {   return start;   }
+    //             start++;
+
+    //             //check if the end is pivot
+    //             if(arr[end] > arr[end-1])
+    //             {   return end-1;   }
+    //             end--;
+    //         }
+
+    //         //left side is sorted , so pivot should be in right
+    //         else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end]))
+    //         {
+    //             start = mid+1;
+    //         }
+    //         else
+    //         {
+    //             end = mid-1;
+    //         }
+    //     }
+    //     return -1;
+    // }
 }
