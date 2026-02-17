1fd640fc2ad3e4b472de7563c8008cd9c04afd2b
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-17 18:35:18 +0530

    Time: 22 ms (43.15%), Space: 13.5 MB (74.48%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,14 +1,26 @@
 class Solution(object):
     def findErrorNums(self, nums):
-        count=Counter(nums)
-        ans=[]
-        for key,value in count.items():
-            if value>1:
-                ans.append(key)
-                break
-        for x in range(1,len(nums)+1):
-            if x not in count.keys():
-                ans.append(x)
-                break
-        return ans
+        # count=Counter(nums)
+        # ans=[]
+        # for key,value in count.items():
+        #     if value>1:
+        #         ans.append(key)
+        #         break
+        # for x in range(1,len(nums)+1):
+        #     if x not in count.keys():
+        #         ans.append(x)
+        #         break
+        # return ans
+
+        duplicate = -1
+        missing = -1
+        for num in nums:
+            index = abs(num)-1
+            if nums[index]<0:
+                duplicate = abs(num)
+            else:nums[index] = -nums[index]
+        for i in range(len(nums)):
+            if nums[i]>0:
+                missing = i+1
+        return [duplicate,missing]
         
