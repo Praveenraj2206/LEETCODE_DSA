9db8d999a5a0a304b2e33cb9e1f8935126adb265
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-19 17:57:15 +0530

    Time: 14 ms (50.89%), Space: 42.3 MB (15.17%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -12,10 +12,8 @@ public class Solution extends VersionControl
             int mid=start+(end-start)/2;
             if (isBadVersion(mid) == false)
                 start = mid+1;
-            else if (isBadVersion(mid) == true)
-                end = mid-1;
             else
-                return mid;
+                end = mid-1;
         }
         return start;
     }
