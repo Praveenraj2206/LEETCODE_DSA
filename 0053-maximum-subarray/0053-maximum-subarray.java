8c2d1fa1eab249c8804970a34e4c5a44b4aebc7a
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-19 18:05:46 +0530

    Time: 1 ms (99.67%), Space: 77.6 MB (18.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,13 +5,9 @@ class Solution
         int currSum = nums[0],maxSum = nums[0];
         for (int i = 1; i < nums.length; i++) 
         {
-            currSum = maxi(nums[i], currSum + nums[i]);
-            maxSum = maxi(maxSum, currSum);
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
         }
         return maxSum;
-    }
-    public int maxi(int a,int b)
-    {
-        return a>b?a:b;
     }
 }
