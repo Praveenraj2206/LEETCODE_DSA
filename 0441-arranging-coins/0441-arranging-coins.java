9768b6982dac088560db7415ce8de8b194a45010
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-22 21:16:38 +0530

    Time: 1 ms (99.96%), Space: 42.7 MB (64.1%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -2,18 +2,18 @@ class Solution
 {
     public int arrangeCoins(int n) 
     {
-        long start=0;
-        long end=n;
+        long start = 0;
+        long end = n;
         while (start <= end)
         {
             long k = start+(end-start)/2;
             long curr = k*(k+1)/2;
-            if (n == curr)
-                return (int) k;
             if (n<curr)
-                end = k-1;
+                end = k -1;
+            else if (n>curr)
+                start = k +1;
             else
-                start = k+1;
+                return (int)k;
         }
         return (int)end;
     }
