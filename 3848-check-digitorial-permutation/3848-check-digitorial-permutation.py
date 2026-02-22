8fb01d1670e8da8fa81766c9da0822bb93e4f960
Praveen Raj S <praveenrajs2005@gmail.com>
2026-02-22 10:01:59 +0530

    Time: 4 ms (80.94%), Space: 12.4 MB (90.47%) - LeetHub

--- 3848-check-digitorial-permutation/3848-check-digitorial-permutation.py
+++ 3848-check-digitorial-permutation/3848-check-digitorial-permutation.py
@@ -1,25 +1,3 @@
-# class Solution(object):
-#     def isDigitorialPermutation(self, n):
-#         def factorial(value):
-#             if value <= 1:
-#                 return 1
-#             return (value)*factorial(value-1)
-            
-#         def summation(value):
-#             sum = 0
-#             while value > 0:
-#                 rem = value%10
-#                 sum += factorial(rem)
-#                 value //= 10
-#             return sum
-#         value = n
-#         sum_n = summation(n)
-#         counter1 = Counter(str(n))
-#         counter2 = Counter(str(sum_n))
-#         if summation(sum_n) == sum_n and counter1 == counter2 and len(str(n)) == len(str(sum_n)):
-#             return True
-#         return False
-
 class Solution(object):
     def isDigitorialPermutation(self, n): 
         # if n==0:
