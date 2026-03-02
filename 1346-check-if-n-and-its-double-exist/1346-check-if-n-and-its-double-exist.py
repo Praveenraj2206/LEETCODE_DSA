b4938a0ae29a3a5c3e2a935b1ee2effb25e646f8
Praveen Raj S <praveenrajs2005@gmail.com>
2026-03-02 19:24:09 +0530

    Time: 12 ms (39.97%), Space: 12.4 MB (74.13%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,9 +1,17 @@
 class Solution(object):
     def checkIfExist(self, arr):
+        arr=sorted(arr)
         for i in range(len(arr)):
             x = arr[i]
-            for j in range(len(arr)):
-                if x == arr[j]*2 and i!=j:
+            element = x*2
+            start,end=0,len(arr)-1
+            while start<=end:
+                mid = start +(end-start)//2
+                if arr[mid] == element and mid != i:
                     return True
+                elif arr[mid] > element:
+                    end = mid - 1
+                else:
+                    start = mid + 1
         return False
         
