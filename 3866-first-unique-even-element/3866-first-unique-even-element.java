e7b97faffdcea521aefb4d066bdedeca01f715fd
Praveen Raj S <praveenrajs2005@gmail.com>
2026-03-14 21:35:02 +0530

    Time: 1 ms (99.25%), Space: 45.5 MB (86.21%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.java
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -2,22 +2,19 @@ class Solution
 {
     public int firstUniqueEven(int[] nums) 
     {
-        List<Integer> evens = new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i] % 2 == 0)
-                evens.add(nums[i]);
-        }
-        for(int i = 0; i < evens.size(); i++)
-        {
-            int count = 0;
-            for(int j = 0; j < nums.length; j++) 
+            if(nums[i]%2==0)
             {
-                if(nums[j] == evens.get(i))
-                    count++;
+                int c=0;
+                for( int j=0;j<nums.length;j++)
+                {
+                    if(nums[i]==nums[j])
+                        c++;
+                }
+            
+            if(c==1)return nums[i];
             }
-            if(count == 1)
-                return evens.get(i);
         }
         return -1;
     }
