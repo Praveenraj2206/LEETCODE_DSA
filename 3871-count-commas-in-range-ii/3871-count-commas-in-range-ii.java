17345718b48f40bb3e64ccaf2b0a43b5db84bd4b
Praveen Raj S <praveenrajs2005@gmail.com>
2026-03-19 09:49:55 +0530

    Time: 1 ms (99.14%), Space: 42.3 MB (99.23%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -2,19 +2,9 @@ class Solution
 {
     public long countCommas(long n) 
     {
-        if(n<=999)
-            return 0;
-        long total_comma = 0;
-        long count_comma = 1;
-        long start = 1000;
-        while(start <= n)
-        {
-            long end = Math.min(n,start*1000-1);
-            long num_range = end - start+1;
-            total_comma += num_range*count_comma;
-            start *= 1000;
-            count_comma++;
-        }
-        return total_comma;
+        long ans =0;
+        for(long i=1000;i<=n;i*=1000)
+            ans += (n-i+1);
+        return ans;
     }
 }
