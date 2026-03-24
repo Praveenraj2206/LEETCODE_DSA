cac7bc6a9aa5af2b7a8a16a7c7b543f3c01670cf
Praveen Raj S <praveenrajs2005@gmail.com>
2026-03-24 19:49:50 +0530

    Time: 1 ms (89.11%), Space: 43.2 MB (80.83%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,24 +1,35 @@
 class Solution 
 {
+    public void countSort(int[] expected)
+    {
+        int[] count = new int[101];
+        for (int num : expected)
+            count[num]++;
+        int j=0;
+        for (int i = 1; i <= 100; i++) 
+        {
+            if(count[i] != 0)
+            {
+                while(count[i]>0)
+                {
+                    expected[j] = i;
+                    j++;
+                    count[i]--;
+                }
+            }
+            if(j == expected.length)
+                break;
+        }
+    }
     public int heightChecker(int[] heights) 
     {
-        int[] freq = new int[101];
-
-        for (int h : heights)
-            freq[h]++;
-
-        int count = 0;
-        int i = 0;
-        for (int h = 1; h <= 100; h++) 
+        int[] expected = heights.clone();
+        countSort(expected);
+        int count=0;
+        for(int i=0;i<expected.length;i++)
         {
-            while (freq[h] > 0) 
-            {
-                if (heights[i] != h)
-                    count++;
-                    
-                i++;        
-                freq[h]--; 
-            }
+            if(expected[i] != heights[i])
+                count++;
         }
         return count;
     }
