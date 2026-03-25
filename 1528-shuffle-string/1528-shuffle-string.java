4b3aa5cf313b38c207863bfff89b64b8649ab7ef
Praveen Raj S <praveenrajs2005@gmail.com>
2026-03-25 19:24:26 +0530

    Time: 1 ms (65.81%), Space: 45.6 MB (69.35%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -3,10 +3,8 @@ class Solution {
         int n = s.length();
         char[] result = new char[n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             result[indices[i]] = s.charAt(i);
-        }
-
         return new String(result);
     }
 }
