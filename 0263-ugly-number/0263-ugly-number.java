3375cbbee58c1d8f6f897a79cbac4d7a4af62611
Praveen Raj S <praveenrajs2005@gmail.com>
2026-03-25 19:58:04 +0530

    Time: 1 ms (99.26%), Space: 42.4 MB (91.2%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -7,7 +7,7 @@ class Solution
         if (n == 0)
             return false;
         int[] factors={2,3,5};
-        for(int i=0;i<factors.length;i++)
+        for(int i=0;i<3;i++)
         {
             while (n % factors[i] == 0)
                 n = n/factors[i];
