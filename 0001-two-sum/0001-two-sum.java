f8b107c6b136d1a1a42d99086ea5f59c0da743e3
Praveen Raj S <praveenrajs2005@gmail.com>
2026-06-15 20:13:44 +0530

    Time: 3 ms (55.71%), Space: 47 MB (72.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,15 +2,68 @@ class Solution
 {
     public int[] twoSum(int[] nums, int target) 
     {
-        Map<Integer,Integer> map= new HashMap<>();
+        Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
-            int compliemt=target-nums[i];
-            if(map.containsKey(compliemt))
-                return new int[] {map.get(compliemt),i};
-            else
-                map.put(nums[i],i);
+            int compliment = target - nums[i];
+            if(map.containsKey(compliment))
+                return new int[]{map.get(compliment), i};
+            map.put(nums[i],i);
         }
-        throw new IllegalArgumentException("NO MATCH");
+        return new int[]{0,0};
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution 
+// {
+//     public int[] twoSum(int[] nums, int target) 
+//     {
+//         Map<Integer,Integer> map= new HashMap<>();
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             int compliemt=target-nums[i];
+//             if(map.containsKey(compliemt))
+//             {
+//                 return new int[] {map.get(compliemt),i};
+//             }
+//             else
+//             {
+//                 map.put(nums[i],i);
+//             }
+//         }
+//         throw new IllegalArgumentException("NO MATCH");
+//     }
+// }
