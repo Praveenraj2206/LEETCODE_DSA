d529dc19582233bb2162b6940f5845ddc9b839f2
Praveen Raj S <praveenrajs2005@gmail.com>
2026-06-23 20:11:52 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,12 +2,33 @@ class Solution
 {
     public int fib(int n) 
     {
-        return solve(n);
-    }
-    public int solve(int n)
-    {
-        if(n<=1)
+        if (n <= 1) 
             return n;
-        return solve(n-1)+solve(n-2);
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) 
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 }
+
+
+
+// class Solution 
+// {
+//     int[] arr;
+//     public int fib(int n) 
+//     {
+//         arr = new int[n+1];
+//         return solve(n);
+//     }
+//     public int solve(int n)
+//     {
+//         if(n<=1)
+//             return n;
+//         return solve(n-1)+solve(n-2);
+//     }
+// }
