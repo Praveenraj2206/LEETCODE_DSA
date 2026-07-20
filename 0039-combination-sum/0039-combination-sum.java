e95730e9be7a4d39fd6f6d1afdd7d7f930855bf5
Praveen Raj S <praveenrajs2005@gmail.com>
2026-07-20 22:37:53 +0530

    Time: 2 ms (96.61%), Space: 45.9 MB (33.93%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,58 @@
 class Solution 
 {
-    List<List<Integer>> list=new ArrayList<>();
+    List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) 
     {
-        sum(candidates,target,0,new ArrayList<>(),0);
+        sum(candidates, target, 0, new ArrayList<>());
         return list;
     }
-    public void sum(int[] candidates,int target,int index,List<Integer> sublist,int sum_value)
+
+    public void sum(int[] candidates, int target, int index, List<Integer> sublist) 
     {
-        if(sum_value == target)
+        if (target == 0) 
         {
             list.add(new ArrayList<>(sublist));
             return;
         }
-        if(index == candidates.length)
+
+        if (target < 0 || index == candidates.length) 
             return;
-        if(sum_value > target)
-            return;
-        sum_value += candidates[index];
+
+        // Take the current candidate
         sublist.add(candidates[index]);
-        sum(candidates,target,index,sublist,sum_value);
-        sum_value -= candidates[index];
-        sublist.remove(sublist.size()-1);
-        sum(candidates,target,index+1,sublist,sum_value);
-        return; 
+        sum(candidates, target - candidates[index], index, sublist);
+        sublist.remove(sublist.size() - 1);
+
+        // Skip the current candidate
+        sum(candidates, target, index + 1, sublist);
     }
-} 
+}
+
+// class Solution 
+// {
+//     List<List<Integer>> list=new ArrayList<>();
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) 
+//     {
+//         sum(candidates,target,0,new ArrayList<>(),0);
+//         return list;
+//     }
+//     public void sum(int[] candidates,int target,int index,List<Integer> sublist,int sum_value)
+//     {
+//         if(sum_value == target)
+//         {
+//             list.add(new ArrayList<>(sublist));
+//             return;
+//         }
+//         if(index == candidates.length)
+//             return;
+//         if(sum_value > target)
+//             return;
+//         sum_value += candidates[index];
+//         sublist.add(candidates[index]);
+//         sum(candidates,target,index,sublist,sum_value);
+//         sum_value -= candidates[index];
+//         sublist.remove(sublist.size()-1);
+//         sum(candidates,target,index+1,sublist,sum_value);
+//         return; 
+//     }
+// } 
