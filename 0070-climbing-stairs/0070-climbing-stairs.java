a5692a49f7fa23d0970093c7b4d1ed085a3ebfcd
Praveen Raj S <praveenrajs2005@gmail.com>
2026-07-25 10:30:55 +0530

    Time: 0 ms (100%), Space: 42 MB (54.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -16,3 +16,13 @@ class Solution
         return b;
     }
 }
+
+// class Solution 
+// {
+//     public int climbStairs(int n) 
+//     {
+//         if(n==0 || n==1)
+//             return 1;
+//         return climbStairs(n-1) + climbStairs(n-2);
+//     }
+// }
