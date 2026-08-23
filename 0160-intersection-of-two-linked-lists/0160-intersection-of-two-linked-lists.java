a55a7578828023b151d8c6cc51877bdeb05a93a7
Praveen Raj S <praveenrajs2005@gmail.com>
2026-08-23 16:43:38 +0530

    Time: 10 ms (12.43%), Space: 52.2 MB (98.26%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -13,48 +13,79 @@ public class Solution
 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
     {
-        int size1 = 0;
-        int size2 = 0;
-        ListNode A = headA;
-        ListNode B = headB;
-        while(A != null)
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode temp=headA;
+        while(!set.contains(temp) && temp != null)
         {
-            A = A.next;
-            size1++;
+            set.add(temp);
+            temp = temp.next;
         }
-        while(B != null)
+        temp = headB;
+        while(temp != null)
         {
-            B = B.next;
-            size2++;
-        }
-        int diff = Math.abs(size1 - size2);
-        if(size1>size2)
-        {
-            A = headA;
-            B = headB;
-            while(diff>0)
-            {
-                A = A.next;
-                diff--;
-            }
-        }
-        else
-        {
-            B = headB;
-            A = headA;
-            while(diff>0)
-            {
-                B = B.next;
-                diff--;
-            }
-        }
-        while(A != null)
-        {
-            if(A == B)
-                return A;
-            A = A.next;
-            B = B.next;
+            if(set.contains(temp))
+                return temp;
+            temp = temp.next;
         }
         return null;
     }
+
+    // public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
+    // {
+    //     ListNode t1 = headA;
+    //     ListNode t2 = headB;
+    //     while(t1 != t2)
+    //     {
+    //         t1 = (t1==null) ? headB : t1.next;
+    //         t2 = (t2==null) ? headA : t2.next;
+    //     }
+    // }
+
+
+    // public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
+    // {
+    //     int size1 = 0;
+    //     int size2 = 0;
+    //     ListNode A = headA;
+    //     ListNode B = headB;
+    //     while(A != null)
+    //     {
+    //         A = A.next;
+    //         size1++;
+    //     }
+    //     while(B != null)
+    //     {
+    //         B = B.next;
+    //         size2++;
+    //     }
+    //     int diff = Math.abs(size1 - size2);
+    //     if(size1>size2)
+    //     {
+    //         A = headA;
+    //         B = headB;
+    //         while(diff>0)
+    //         {
+    //             A = A.next;
+    //             diff--;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         B = headB;
+    //         A = headA;
+    //         while(diff>0)
+    //         {
+    //             B = B.next;
+    //             diff--;
+    //         }
+    //     }
+    //     while(A != null)
+    //     {
+    //         if(A == B)
+    //             return A;
+    //         A = A.next;
+    //         B = B.next;
+    //     }
+    //     return null;
+    // }
 }
