dfe97531acc62f52d8284615fc0833131af3f236
Praveen Raj S <praveenrajs2005@gmail.com>
2026-09-02 19:13:52 +0530

    Time: 4 ms (99.02%), Space: 46.9 MB (99.81%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,46 @@
+class Solution 
+{
+    public boolean validPalindrome(String s) 
+    {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) 
+        {
+            if (s.charAt(left) != s.charAt(right)) 
+            {
+                boolean skipLeft = true;
+                int l = left + 1;
+                int r = right;
+                while (l < r) 
+                {
+                    if (s.charAt(l) != s.charAt(r)) 
+                    {
+                        skipLeft = false;
+                        break;
+                    }
+                    l++;
+                    r--;
+                }
+
+                boolean skipRight = true;
+                l = left;
+                r = right - 1;
+                while (l < r) 
+                {
+                    if (s.charAt(l) != s.charAt(r)) 
+                    {
+                        skipRight = false;
+                        break;
+                    }
+                    l++;
+                    r--;
+                }
+                return skipLeft || skipRight;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
