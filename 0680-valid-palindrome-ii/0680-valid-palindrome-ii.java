5d0b14364ea9fb5b29334e13865f19b1bec6cc34
Praveen Raj S <praveenrajs2005@gmail.com>
2026-09-02 19:16:26 +0530

    Time: 4 ms (99.02%), Space: 47.6 MB (85.87%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -8,39 +8,74 @@ class Solution
         {
             if (s.charAt(left) != s.charAt(right)) 
             {
-                boolean skipLeft = true;
-                int l = left + 1;
-                int r = right;
-                while (l < r) 
-                {
-                    if (s.charAt(l) != s.charAt(r)) 
-                    {
-                        skipLeft = false;
-                        break;
-                    }
-                    l++;
-                    r--;
-                }
-
-                boolean skipRight = true;
-                l = left;
-                r = right - 1;
-                while (l < r) 
-                {
-                    if (s.charAt(l) != s.charAt(r)) 
-                    {
-                        skipRight = false;
-                        break;
-                    }
-                    l++;
-                    r--;
-                }
-                return skipLeft || skipRight;
+                return isPalindrome(s, left + 1, right) || 
+                       isPalindrome(s, left, right - 1);
             }
             left++;
             right--;
         }
+        return true;
+    }
 
+    private boolean isPalindrome(String s, int left, int right) 
+    {
+        while (left < right) 
+        {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
         return true;
     }
 }
+
+
+
+
+// class Solution 
+// {
+//     public boolean validPalindrome(String s) 
+//     {
+//         int left = 0;
+//         int right = s.length() - 1;
+//         while (left < right) 
+//         {
+//             if (s.charAt(left) != s.charAt(right)) 
+//             {
+//                 boolean skipLeft = true;
+//                 int l = left + 1;
+//                 int r = right;
+//                 while (l < r) 
+//                 {
+//                     if (s.charAt(l) != s.charAt(r)) 
+//                     {
+//                         skipLeft = false;
+//                         break;
+//                     }
+//                     l++;
+//                     r--;
+//                 }
+
+//                 boolean skipRight = true;
+//                 l = left;
+//                 r = right - 1;
+//                 while (l < r) 
+//                 {
+//                     if (s.charAt(l) != s.charAt(r)) 
+//                     {
+//                         skipRight = false;
+//                         break;
+//                     }
+//                     l++;
+//                     r--;
+//                 }
+//                 return skipLeft || skipRight;
+//             }
+//             left++;
+//             right--;
+//         }
+
+//         return true;
+//     }
+// }
