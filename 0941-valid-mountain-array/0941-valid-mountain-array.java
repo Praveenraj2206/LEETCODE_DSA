b9b17b51f3274fd5cb0b766ee95327541f091690
Praveen Raj S <praveenrajs2005@gmail.com>
2026-09-08 20:38:05 +0530

    Time: 2 ms (82.89%), Space: 47.2 MB (87.95%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,30 +1,74 @@
-class Solution 
+class Solution
+
 {
-    public boolean validMountainArray(int[] arr) 
+
+public boolean validMountainArray(int[] arr) 
+
+{
+
+    if(arr.length<3)             
+        return false;
+
+    int i=1;
+
+    if(arr[i]<arr[i-1])          
+        return false;
+
+    while(i<arr.length)
+
     {
-        if(arr.length<3)
+
+        if(arr[i]==arr[i-1])     
             return false;
-        int i=1;
-        if(arr[i]<arr[i-1])
-            return false;
-        while(i<arr.length)
-        {
-            if(arr[i]==arr[i-1])
-                return false;
-            if(!(arr[i]>arr[i-1]))
-                break;
-            i++;
-        }
-        if(i==arr.length)
-            return false;
-        while(i<arr.length)
-        {
-            if(arr[i]==arr[i-1])
-                return false;
-            if(!(arr[i]<arr[i-1]))
-                return false;
-            i++;
-        }
-        return true;
+
+        if(!(arr[i]>arr[i-1]))   
+            break;
+
+        i++;
+
     }
+
+    if(i==arr.length)   
+        return false;
+
+    while(i<arr.length)
+
+    {
+
+        if(arr[i]==arr[i-1])    
+            return false;
+
+        if(!(arr[i]<arr[i-1]))  
+            return false;
+
+        i++;
+
+    }
+
+    return true;
+
 }
+}
+
+
+// class Solution 
+// {
+//     public boolean validMountainArray(int[] arr) 
+//     {
+//         if (arr.length < 3)
+//             return false;
+//         int i = 1;
+//         if (arr[i] < arr[i - 1])
+//             return false;
+
+//         while (i < arr.length && arr[i] > arr[i - 1])
+//             i++;
+
+//         if (i == arr.length)
+//             return false;
+
+//         while (i < arr.length && arr[i] < arr[i - 1])
+//             i++;
+//         return i == arr.length;
+//     }
+// }
