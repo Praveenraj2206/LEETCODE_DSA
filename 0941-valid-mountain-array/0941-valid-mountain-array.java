101219ae20d4f1d840874b7d4eebd07038805555
Praveen Raj S <praveenrajs2005@gmail.com>
2026-09-08 20:40:40 +0530

    Time: 3 ms (9.99%), Space: 47.5 MB (34.09%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,55 +1,26 @@
 class Solution
-
 {
-
-public boolean validMountainArray(int[] arr) 
-
-{
-
-    if(arr.length<3)             
-        return false;
-
-    int i=1;
-
-    if(arr[i]<arr[i-1])          
-        return false;
-
-    while(i<arr.length)
-
+    public boolean validMountainArray(int[] arr)
     {
-
-        if(arr[i]==arr[i-1])     
-            return false;
-
-        if(!(arr[i]>arr[i-1]))   
-            break;
-
-        i++;
-
+        if(arr.length<3) return false;
+        int i=1;
+        if(arr[i]<arr[i-1]) return false;
+        while(i<arr.length)
+        {
+            if(arr[i]==arr[i-1]) return false;
+            if(!(arr[i]>arr[i-1])) break;
+            i++;
+        }
+        if(i==arr.length) return false;
+        while(i<arr.length)
+        {
+            if(arr[i]==arr[i-1]) return false;
+            if(!(arr[i]<arr[i-1])) return false;
+            i++;
+        }
+        return true;
     }
-
-    if(i==arr.length)   
-        return false;
-
-    while(i<arr.length)
-
-    {
-
-        if(arr[i]==arr[i-1])    
-            return false;
-
-        if(!(arr[i]<arr[i-1]))  
-            return false;
-
-        i++;
-
-    }
-
-    return true;
-
 }
-}
-
 
 // class Solution 
 // {
