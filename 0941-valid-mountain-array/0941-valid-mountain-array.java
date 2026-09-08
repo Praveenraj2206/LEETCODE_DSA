18c2ce4008a2aceca79ab632bf78d51d4418a82d
Praveen Raj S <praveenrajs2005@gmail.com>
2026-09-08 20:41:19 +0530

    Time: 1 ms (99.92%), Space: 47.3 MB (73.86%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,45 +1,46 @@
-class Solution
+class Solution 
 {
-    public boolean validMountainArray(int[] arr)
+    public boolean validMountainArray(int[] arr) 
     {
-        if(arr.length<3) return false;
-        int i=1;
-        if(arr[i]<arr[i-1]) return false;
-        while(i<arr.length)
-        {
-            if(arr[i]==arr[i-1]) return false;
-            if(!(arr[i]>arr[i-1])) break;
+        if (arr.length < 3)
+            return false;
+        int i = 1;
+        if (arr[i] < arr[i - 1])
+            return false;
+
+        while (i < arr.length && arr[i] > arr[i - 1])
             i++;
-        }
-        if(i==arr.length) return false;
-        while(i<arr.length)
-        {
-            if(arr[i]==arr[i-1]) return false;
-            if(!(arr[i]<arr[i-1])) return false;
+
+        if (i == arr.length)
+            return false;
+
+        while (i < arr.length && arr[i] < arr[i - 1])
             i++;
-        }
-        return true;
+        return i == arr.length;
     }
 }
 
-// class Solution 
+
+// class Solution
 // {
-//     public boolean validMountainArray(int[] arr) 
+//     public boolean validMountainArray(int[] arr)
 //     {
-//         if (arr.length < 3)
-//             return false;
-//         int i = 1;
-//         if (arr[i] < arr[i - 1])
-//             return false;
-
-//         while (i < arr.length && arr[i] > arr[i - 1])
+//         if(arr.length<3) return false;
+//         int i=1;
+//         if(arr[i]<arr[i-1]) return false;
+//         while(i<arr.length)
+//         {
+//             if(arr[i]==arr[i-1]) return false;
+//             if(!(arr[i]>arr[i-1])) break;
 //             i++;
-
-//         if (i == arr.length)
-//             return false;
-
-//         while (i < arr.length && arr[i] < arr[i - 1])
+//         }
+//         if(i==arr.length) return false;
+//         while(i<arr.length)
+//         {
+//             if(arr[i]==arr[i-1]) return false;
+//             if(!(arr[i]<arr[i-1])) return false;
 //             i++;
-//         return i == arr.length;
+//         }
+//         return true;
 //     }
 // }
